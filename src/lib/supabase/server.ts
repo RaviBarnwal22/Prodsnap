b0241d1894c8941be8910cd382cb956d2f66e51c
RaviBarnwal22 <119891747+RaviBarnwal22@users.noreply.github.com>
2026-01-23 14:09:17 +0530

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -20,9 +20,7 @@ export async function createClient() {
                                 ...options,
                                 path: '/',
                                 sameSite: 'lax' as const,
-                                secure: true,
-                                httpOnly: true,
-                                maxAge: 60 * 60 * 24 * 365,
+                                
                             }
                             cookieStore.set(name, value, cookieOptions)
                         })
