7331ebce004b0c238552d27a1f3d7b607d3156c6
RaviBarnwal22 <119891747+RaviBarnwal22@users.noreply.github.com>
2026-01-23 14:08:44 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -55,7 +55,9 @@ export async function middleware(request: NextRequest) {
                             ...options,
                             path: '/',
                             sameSite: 'lax' as const,
-                            
+                            secure: true,
+                            httpOnly: true,
+                            maxAge: 60 * 60 * 24 * 365,
                         }
                         response.cookies.set(name, value, cookieOptions)
                     })
